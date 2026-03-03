5d1acd7656dd7f5c351e7685affe4b70967f000b
a23rogeliofd <iq200x@protonmail.com>
2026-03-03 10:45:00 +0100

    Modificado

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,20 @@
 plugins {
-    kotlin("jvm") version "2.2.20"
-    kotlin("plugin.serialization") version "2.2.20"
+    kotlin("jvm") version "2.0.20" // Ajustado a versión estable común
+    kotlin("plugin.serialization") version "2.0.20"
+    application // <--- 1. AGREGA ESTO
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-// --- ESTO ES LO QUE TE FALTA ---
 repositories {
-    mavenCentral() // Aquí es donde Gradle buscará las librerías
+    mavenCentral()
 }
 
 dependencies {
-    // La librería para el JSON
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+}
+
+application {
+    mainClass.set("org.example.MainKt")
 }
